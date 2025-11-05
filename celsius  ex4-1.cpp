8943716cfebc021011e4a542a4b55dbfb660c5c3
Harshithapulla2007 <harshithapulla933@gmail.com>
2025-11-05 20:23:39 +0530

    Add files via upload

---
+++ celsius  ex4-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+	int c,conversion;
+	printf ("enter the values");
+	scanf ("%d",&c);
+	conversion=(c* 9/5)+32;
+	printf("%d",conversion);
+	printf("% d",conversion);
+	return 0;
+}
